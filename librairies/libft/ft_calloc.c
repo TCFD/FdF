40594e770f91b80324ee0c19254ca2aa843872e0
TCFD <r.ciaze@gmail.com>
2023-10-16 12:22:54 +0200

    rm .o

--- librairies/libft/ft_calloc.c
+++ librairies/libft/ft_calloc.c
@@ -6,24 +6,21 @@
 /*   By: rciaze <rciaze@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:34:54 by rciaze            #+#    #+#             */
-/*   Updated: 2022/11/25 19:53:24 by rciaze           ###   ########.fr       */
+/*   Updated: 2023/10/04 14:17:04 by rciaze           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "../../includes/minishell.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*re;
 
 	if (nmemb == 0 || size == 0)
-		return (malloc(0));
+		return (ft_malloc(0));
 	if (__SIZE_MAX__ / nmemb > size)
-	{
-		re = malloc(nmemb * size);
-		if (!re)
-			return (NULL);
-	}
+		re = ft_malloc(nmemb * size);
 	else
 		return (NULL);
 	ft_bzero(re, nmemb * size);
