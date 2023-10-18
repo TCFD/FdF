b14659ac8145703e1aa31fd14a19f2f9ee4cf8ac
TCFD <r.ciaze@gmail.com>
2023-10-18 17:27:52 +0200

    j'avais un probleme avec ft_malloc dans callco

--- librairies/libft/ft_calloc.c
+++ librairies/libft/ft_calloc.c
@@ -6,21 +6,20 @@
 /*   By: rciaze <rciaze@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:34:54 by rciaze            #+#    #+#             */
-/*   Updated: 2023/10/04 14:17:04 by rciaze           ###   ########.fr       */
+/*   Updated: 2023/10/18 14:59:24 by rciaze           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "../../includes/minishell.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*re;
 
 	if (nmemb == 0 || size == 0)
-		return (ft_malloc(0));
+		return (malloc(0));
 	if (__SIZE_MAX__ / nmemb > size)
-		re = ft_malloc(nmemb * size);
+		re = malloc(nmemb * size);
 	else
 		return (NULL);
 	ft_bzero(re, nmemb * size);
